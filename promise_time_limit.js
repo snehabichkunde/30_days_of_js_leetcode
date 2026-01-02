74e72b1910b7447af7f3478fe45c47d97a412b04
sneha bichkunde <bichkundesneha@gmail.com>
2026-01-02 12:43:15 +0530

    learned about how to creat the stopwatch i.e. Promise.race([task, timeout])

---
+++ promise_time_limit.js
@@ -0,0 +1,105 @@
+// Given an asynchronous function fn and a time t in milliseconds, return a new time limited version of the input function. fn takes arguments provided to the time limited function.
+
+// The time limited function should follow these rules:
+
+// If the fn completes within the time limit of t milliseconds, the time limited function should resolve with the result.
+// If the execution of the fn exceeds the time limit, the time limited function should reject with the string "Time Limit Exceeded".
+ 
+
+// Example 1:
+
+// Input: 
+// fn = async (n) => { 
+//   await new Promise(res => setTimeout(res, 100)); 
+//   return n * n; 
+// }
+// inputs = [5]
+// t = 50
+// Output: {"rejected":"Time Limit Exceeded","time":50}
+// Explanation:
+// const limited = timeLimit(fn, t)
+// const start = performance.now()
+// let result;
+// try {
+//    const res = await limited(...inputs)
+//    result = {"resolved": res, "time": Math.floor(performance.now() - start)};
+// } catch (err) {
+//    result = {"rejected": err, "time": Math.floor(performance.now() - start)};
+// }
+// console.log(result) // Output
+
+// The provided function is set to resolve after 100ms. However, the time limit is set to 50ms. It rejects at t=50ms because the time limit was reached.
+// Example 2:
+
+// Input: 
+// fn = async (n) => { 
+//   await new Promise(res => setTimeout(res, 100)); 
+//   return n * n; 
+// }
+// inputs = [5]
+// t = 150
+// Output: {"resolved":25,"time":100}
+// Explanation:
+// The function resolved 5 * 5 = 25 at t=100ms. The time limit is never reached.
+
+
+
+// Approch one more manual approch 
+
+
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit1 = function(fn, t) {
+    
+    return async function(...args) {
+        return new Promise ((resolve, reject)=>{
+            const timer = setTimeout(()=>{
+                reject("Time Limit Exceeded");
+            }, t);
+
+            fn(...args)
+                .then((res)=>{
+                    clearTimeout(timer);
+                    resolve(res);
+                })
+                .catch((err)=>{
+                    clearTimeout(timer);
+                    reject(err);
+                })
+        }) 
+    }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
+
+
+// apprach 2 => using promise.race i.e. creating a staopwatch 
+
+
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+    
+    return async function(...args) {
+        const timeoutPromise = new Promise ((_, reject)=>{
+            setTimeout(()=>{
+                reject("Time Limit Exceeded");
+            }, t);
+        })
+        return Promise.race([fn(...args), timeoutPromise]); 
+    }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
